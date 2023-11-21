9bfe847968930e8a693661697cfa824d3c79142f
Denis Gladky <de.gladkiy@gmail.com>
2023-11-21 10:28:15 +0300

    init

--- bin/socket_test.dart
+++ bin/socket_test.dart
@@ -8,7 +8,7 @@ void startServer() {
     server.listen((Socket socket) {
       socket.listen((List<int> data) {
         String result = String.fromCharCodes(data);
-        print(result);
+        print('$result\n');
       });
     });
   });
